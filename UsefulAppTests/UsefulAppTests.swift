015852592d2076604fe47afb54faba0c502b7fc4
Evan Meyer <evan.meyer26@d155.org>
2024-10-28 08:14:09 -0500

    Initial Commit

---
+++ UsefulAppTests/UsefulAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulAppTests.swift
+//  UsefulAppTests
+//
+//  Created by EVAN MEYER on 10/28/24.
+//
+
+import Testing
+@testable import UsefulApp
+
+struct UsefulAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
